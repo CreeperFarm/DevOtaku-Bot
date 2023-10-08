2a2247747185aab6f81ae622f3c04deafc99871d
CreeperFarm <creeperfarm.pro@gmail.com>
2023-10-08 17:33:47 +0200

    Update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const {Client, Intents} = require('discord.js');
+const {Client, Events, GatewayIntentBits} = require('discord.js');
 const fs = require('fs');
-const client = new Client({intents: []});
+const client = new Client({intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]});
 
 let rawdata = fs.readFileSync('config.json');
 let config = JSON.parse(rawdata);
@@ -13,22 +13,27 @@ client.on("ready", () => {
     console.log("Logged in as " + client.user.tag + "! The prefix is " + prefix + " .");
 });
 
-client.on('message', msg => {
+client.on('messageCreate', msg => {
 
-    console.print(msg.content);
+    console.log(msg.content);
 
     if (msg.author.bot) return;
 
     if (msg.content === prefix + "ping") {
         msg.reply("Pong!");
-    };
-});
+    }
 
-client.on('guildMemberAdd', member => {
-    member.createDM().then(channel => {
-        return channel.send('Bienvenue sur mon serveur ' + member.displayName)
-    }).catch(console.error)
-    // On pourrait catch l'erreur autrement ici (l'utilisateur a peut être désactivé les MP)
+    if (message.content == prefix + "help"){
+        const embed = new Discord.MessageEmbed()
+           .setColor("#B072FF")
+           .setTitle("Liste des commandes:")
+           .setAuthor("CreeperFarm", "https://avatars.githubusercontent.com/u/62711198?s=96&v=4", "https://github.com/CreeperFarm")
+           .addField(prefix + "projet", "Donne le lien du projet CreeperAnime")
+           .addField(prefix + "anime", "Permet la sélection d'un anime")
+           .addField(prefix + "", "Add soon ...");
+  
+        message.reply({ embeds: [embed]});
+    }
 });
 
 client.login(TOKEN)
